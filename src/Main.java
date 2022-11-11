5c8e2d8d63e6d8a343cabbe4338908fdbf39a25d
ilyakol <ilja-kolesnikov@mail.ru>
2022-11-11 23:35:31 +0300

    qwe

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,6 @@ public class Main {
                 .until(ExpectedConditions.elementToBeClickable(By.className("ruhjFe")));
         wiki.click();
         driver.quit();
+        //new
     }
 }
